65aa2109cc138f47302df40aa260b9442b4ef01a
oluwapelumi1 <oluwapelumidaniel88@gmail.com>
2025-11-26 11:18:52 +0100

    commit

--- app/(web)/weekly-menu/page.tsx
+++ app/(web)/weekly-menu/page.tsx
@@ -2,26 +2,34 @@ import FAQ from "@/components/globals/Faq";
 import Meals from "@/components/globals/meals";
 import Testimonials from "@/components/PageLayout/Home/Testimonials";
 import WeeklyHero from "@/components/PageLayout/WeeklyMenu/WeeklyHero";
-import { Plus } from "lucide-react";
-import TestRecipe from "@/components/TestRecipe";
 import React from "react";
 
-export default function page() {
+export default function WeeklyMenuPage() {
   return (
-    <div>
+    <div className="w-full">
+
+      {/* Hero Section */}
       <div className="w-full min-h-screen bg-[#FFF9F0]">
         <WeeklyHero />
 
+        {/* Meals Section */}
         <div
-          className="container mx-auto px-5 md:px-[97px] pb-16 "
+          className="container mx-auto px-5 md:px-[97px] pb-16"
           id="meals-section"
         >
           <Meals />
         </div>
       </div>
-      <Testimonials />
-       <TestRecipe />;
-      <FAQ />
+
+      {/* Testimonials */}
+      <div className="mt-10">
+        <Testimonials />
+      </div>
+
+      {/* FAQ */}
+      <div className="mt-10">
+        <FAQ />
+      </div>
     </div>
   );
 }
