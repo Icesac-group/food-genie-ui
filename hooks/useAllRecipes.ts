30d67524259b93ea2e0e48db50dc2988eb64c985
oluwapelumi1 <oluwapelumidaniel88@gmail.com>
2025-12-01 14:30:00 +0100

    amend

--- hooks/useAllRecipes.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { recipeService, Recipe } from '@/lib/api/services';
-
-export const useAllRecipes = () => {
-  return useQuery<Recipe[], Error>({
-    queryKey: ['recipes'],
-    queryFn: recipeService.getAll,
-  });
-};
